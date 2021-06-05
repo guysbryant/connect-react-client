fc2b7d9e6cb1827757a2783f51edecb014296a9b
guysbryant <guy.s.bryant@gmail.com>
2021-06-05 18:32:59 -0400

    Add CircleListItem and CirclesList files

---
+++ src/components/CirclesList.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const CirclesList = () =>{
+    return(
+        <>
+          <h1>Circles List</h1>
+            <ul>
+            </ul>
+        </>
+    )
+}
+
+export default CirclesList
