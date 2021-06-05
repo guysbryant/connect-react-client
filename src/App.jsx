78c6cc7ee66161c74edcaaaa39782a86f05c9339
guysbryant <guy.s.bryant@gmail.com>
2021-06-05 18:30:19 -0400

    rename extension

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import { BrowserRouter as Router, Switch, Route, Link, NavLink } from "react-router-dom"
+import Circles from './containers/Circles'
+import CircleForm from './containers/CircleForm'
+
+function App() {
+  return (
+    <div className="App">
+        <Router>
+            <nav className="text-center bg-blue-900 text-yellow-100 p-4" >
+              <NavLink
+                    className="inline-block px-4 py-2"
+                    exact
+                    to="/"
+                    activeClassName="text-yellow-300"
+                >
+                    Circles
+              </NavLink>
+              <NavLink
+                    className="inline-block px-4 py-2"
+                    to="/circles/new"
+                    activeClassName="text-yellow-300"
+                >
+                    New Circles
+              </NavLink>
+            </nav>
+          <Switch>
+            <Route exact path="/">
+              <Circles />
+            </Route>
+            <Route path="/circles/new">New Circle</Route>
+          </Switch>
+        </Router>
+    </div>
+);
+}
+
+export default App;
