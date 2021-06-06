012c41ef328110c3ad74b4dadceae9199ebd4540
guysbryant <guy.s.bryant@gmail.com>
2021-06-06 09:25:01 -0400

    Build CircleForm with fetch post to api with some styaling

---
+++ src/containers/CircleForm.jsx
@@ -0,0 +1,46 @@
+import React from 'react'
+
+export default class CircleForm extends React.Component{
+    state={
+        name: ''
+    }
+
+    submitHandler = (event) =>{
+        event.preventDefault()
+        fetch('http://localhost:3001/circles',{
+            method: 'Post',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({circle: this.state})
+        })
+            .then(response => response.json())
+            .then(circle => {
+                debugger
+            })
+    }
+
+    changeHandler = (event) =>{
+        const { name, value } = event.target
+        this.setState({[name]: value})
+    }
+
+    render(){
+        return(
+            <form onSubmit={this.submitHandler} className="max-w-6xl w-3/4 mx-auto mt-16 shadow-lg-4 py-6">
+              <h1 className="text-center text-2xl font-semibold mb-2">New Circle</h1>
+              <fieldset>
+                <input type="text"
+                       name="name"
+                       onChange={this.changeHandler}
+                       value={this.state.name}
+                       placeholder="Circle Name"
+                       className="w-full border p-4 my-2"
+                />
+              </fieldset>
+              <button className="w-full p-4 bg-blue-300 mt-4 hover:bg-blue-400 transition:all duration-200" type="submit">Add Circle</button>
+            </form>
+        )
+    }
+}
