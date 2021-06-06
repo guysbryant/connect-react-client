c13a27fc0ec48968ec514cc489530e8bb74a31c5
guysbryant <guy.s.bryant@gmail.com>
2021-06-06 17:35:36 -0400

    Add fetch to dynamic groupId

--- src/containers/CircleShow.jsx
+++ src/containers/CircleShow.jsx
@@ -2,6 +2,15 @@ import React from 'react'
 
 export default class CircleShow extends React.Component{
 
+    componentDidMount = () =>{
+        const circleId = this.props.match.params.circleId
+        fetch(`http://localhost:3001/circles/${circleId}`)
+                .then(response => response.json())
+                .then(circle =>{
+                    console.log(circle)
+        })
+    }
+
     render(){
         return(
             <section>Circle Show</section>
