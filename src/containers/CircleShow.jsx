a1415941bd34650deb140b040117b10ced70884e
guysbryant <guy.s.bryant@gmail.com>
2021-06-06 17:28:11 -0400

    Add CircleShow component

---
+++ src/containers/CircleShow.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default class CircleShow extends React.Component{
+
+    render(){
+        return(
+            <section>Circle Show</section>
+        )
+    }
+}
