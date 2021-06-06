aafd76f7242594f07f925b337b121c182ddca299
guysbryant <guy.s.bryant@gmail.com>
2021-06-06 09:23:23 -0400

    Accept props, map over props and render CircleListItem s passing props

--- src/components/CirclesList.jsx
+++ src/components/CirclesList.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import CircleListItem from './CircleListItem'
 
-const CirclesList = () =>{
+const CirclesList = ({circles}) =>{
     return(
         <>
           <h1>Circles List</h1>
             <ul>
+              {circles.map(circle => <CircleListItem key={circle.id} circle={circle} />)}
             </ul>
         </>
     )
