c3e22937fbc76c5885427e86701b518991df0697
guysbryant <guy.s.bryant@gmail.com>
2021-06-06 18:45:16 -0400

    Successfully fetching circles and thier posts from the server

--- src/containers/CircleShow.jsx
+++ src/containers/CircleShow.jsx
@@ -1,17 +1,26 @@
 import React from 'react'
 
 export default class CircleShow extends React.Component{
+    state={
+        circle: {},
+        posts: [],
+        loading: true
+    }
 
     componentDidMount = () =>{
         const circleId = this.props.match.params.circleId
         fetch(`http://localhost:3001/circles/${circleId}`)
                 .then(response => response.json())
-                .then(circle =>{
-                    console.log(circle)
+            .then(({ circle, posts }) =>{
+                    this.setState({circle, posts, loading: false})
+                    console.log(this.state)
         })
     }
 
     render(){
+        if (this.state.loading){
+            return <div>Loading Spinner</div>
+        }
         return(
             <section>Circle Show</section>
         )
