d4e74e4441d467a1112187ae0780ae4dac1c8679
guysbryant <guy.s.bryant@gmail.com>
2021-06-07 21:24:15 -0400

    Add a link to create a new post associated with its circle

--- src/containers/CircleShow.jsx
+++ src/containers/CircleShow.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default class CircleShow extends React.Component{
     state={
@@ -10,10 +11,10 @@ export default class CircleShow extends React.Component{
     componentDidMount = () =>{
         const circleId = this.props.match.params.circleId
         fetch(`http://localhost:3001/circles/${circleId}`)
-                .then(response => response.json())
+            .then(response => response.json())
             .then(({ circle, posts }) =>{
-                    this.setState({circle, posts, loading: false})
-        })
+                this.setState({circle, posts, loading: false})
+            })
     }
 
     render(){
@@ -23,6 +24,7 @@ export default class CircleShow extends React.Component{
         return(
             <section className="max-w-6xl w-11/12 mx-auto mt-16">
               <h1 className="text-3xl font-bold text-center mb-8">{this.state.circle.name}</h1>
+              <p className="my-2"><Link className="border-4" to={`/circles/${this.state.circle.id}/posts/new`}>Create New Post</Link></p>
               <div className="">
                 {this.state.posts.map(post =>
                     <p>{post.text}</p>
