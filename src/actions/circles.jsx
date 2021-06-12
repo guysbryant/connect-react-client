e8105796ef5b938ec45407c4543270a2dce6e447
guysbryant <guy.s.bryant@gmail.com>
2021-06-12 10:18:42 -0400

    Add fetch for a circle

--- src/actions/circles.jsx
+++ src/actions/circles.jsx
@@ -16,6 +16,22 @@ export const fetchCircles = () => {
                     payload: circles
                 })
         })
-        
+    }
+}
+
+export const fetchCircle = (circleId) => {
+    return (dispatch) => {
+        dispatch({type: LOADING_CIRCLES})
+        fetch(`http://localhost:3001/circles${circleId}`, {
+            method: 'get',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            }
+        })
+            .then(response => response.json())
+            .then(({ circle, posts }) =>{
+                this.setState({circle, posts, loading: false})
+        })
     }
 }
