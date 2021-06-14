a785d984c070e7d69902b19f868b1f6c1e0f502a
guysbryant <guy.s.bryant@gmail.com>
2021-06-13 21:56:21 -0400

    Move navbar to its own component and render it in App

--- src/App.jsx
+++ src/App.jsx
@@ -4,28 +4,13 @@ import Circles from './containers/Circles'
 import CircleForm from './containers/CircleForm'
 import CircleShow from './containers/CircleShow'
 import NewPost from './containers/NewPost'
+import NavBar from './components/NavBar'
 
 function App() {
     return (
         <div className="App">
           <Router>
-            <nav className="text-center bg-blue-900 text-yellow-100 p-4" >
-              <NavLink
-                className="inline-block px-4 py-2"
-                exact
-                to="/"
-                activeClassName="text-yellow-300"
-              >
-                Circles
-              </NavLink>
-              <NavLink
-                className="inline-block px-4 py-2"
-                to="/circles/new"
-                activeClassName="text-yellow-300"
-              >
-                New Circles
-              </NavLink>
-            </nav>
+            <NavBar />
             <Switch>
               <Route exact path="/">
                 <Circles />
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import { BrowserRouter as Router, Switch, Route, Link, NavLink } from "react-router-dom"
+
+const NavBar = () => {
+    return(
+        <>
+          <nav className="text-center bg-blue-900 text-yellow-100 p-4" >
+            <NavLink
+              className="inline-block px-4 py-2"
+              exact
+              to="/"
+              activeClassName="text-yellow-300"
+            >
+              Circles
+            </NavLink>
+            <NavLink
+              className="inline-block px-4 py-2"
+              to="/circles/new"
+              activeClassName="text-yellow-300"
+            >
+              New Circles
+            </NavLink>
+          </nav>
+        </>
+    )
+}
+
+export default NavBar
