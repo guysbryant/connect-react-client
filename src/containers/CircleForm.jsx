7f26b7cf8339081ad85f2d8ebc33366a57a86da4
guysbryant <guy.s.bryant@gmail.com>
2021-06-13 21:58:40 -0400

    Wire up to connect, replace fetch with dispatch createCircle

--- src/containers/CircleForm.jsx
+++ src/containers/CircleForm.jsx
@@ -1,23 +1,23 @@
 import React from 'react'
+import { connect } from 'react-redux'
+import {createCircle} from '../actions/circles'
 
-export default class CircleForm extends React.Component{
+class CircleForm extends React.Component{
     state={
-        name: ''
+        name: '',
+        errors: {}
     }
 
     submitHandler = (event) =>{
         event.preventDefault()
-        fetch('http://localhost:3001/circles',{
-            method: 'Post',
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({circle: this.state})
-        })
-            .then(response => response.json())
+        this.props.createCircle(this.state)
             .then(circle => {
                 this.props.history.push('/')
+            })
+            .catch(errors => {
+                this.setState({
+                    errors
+                })
             })
     }
 
@@ -31,12 +31,13 @@ export default class CircleForm extends React.Component{
             <form onSubmit={this.submitHandler} className="max-w-6xl w-3/4 mx-auto mt-16 shadow-lg-4 py-6">
               <h1 className="text-center text-3xl font-semibold mb-2">New Circle</h1>
               <fieldset>
+                <p className="h-8 pl-4 text-red-400">{this.state.errors.name}</p>
                 <input type="text"
                        name="name"
                        onChange={this.changeHandler}
                        value={this.state.name}
                        placeholder="Circle Name"
-                       className="w-full border p-4 my-2"
+                       className={'w-full border-2 focus:outline-none focus:ring-2 p-4 mb-4 ${this.state.errors.name && "focus:ring-red-400 border-red-400"}'}
                 />
               </fieldset>
               <button className="w-full p-4 bg-blue-300 mt-4 hover:bg-blue-400 transition:all duration-200" type="submit">Add Circle</button>
@@ -44,3 +45,11 @@ export default class CircleForm extends React.Component{
         )
     }
 }
+
+const mapDispatchToProps = (dispatch) => {
+    return{
+        createCircle: (formData) => dispatch(createCircle(formData))
+    }
+}
+
+export default connect(null, mapDispatchToProps)(CircleForm)
