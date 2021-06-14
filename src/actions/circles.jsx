3c3980af7b35ce7db8c43eea6437e0b53da12868
guysbryant <guy.s.bryant@gmail.com>
2021-06-13 21:57:10 -0400

    Add and clean up imported actions. Add createCircle action.

--- src/actions/circles.jsx
+++ src/actions/circles.jsx
@@ -1,4 +1,12 @@
-import { LOADING_CIRCLES, LOADED_CIRCLES, LOADING_CIRCLE, LOADED_CIRCLE, LOADED_CIRCLE_POSTS} from './'
+import { LOADING_CIRCLES,
+         LOADED_CIRCLES,
+         LOADING_CIRCLE,
+         LOADED_CIRCLE,
+         LOADED_CIRCLE_POSTS,
+         CREATED_CIRCLE,
+         ERROR_CREATING_CIRCLE
+       } from './'
+
 export const fetchCircles = () => {
     return (dispatch) => {
         dispatch({type: LOADING_CIRCLES})
@@ -36,5 +44,29 @@ export const fetchCircle = (circleId) => {
                     payload: circlePosts
                 })
         })
+    }
+}
+
+export const createCircle = (formData) => {
+    return (dispatch) => {
+        return fetch('http://localhost:3001/circles', {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({circle: formData})
+        })
+            .then(response => {
+                if (response.ok){
+                    return response.json()
+                }else{
+                    return response.json().then(errors => Promise.reject(errors))
+                }
+            })
+            .then(circle => {
+                dispatch({ type: CREATED_CIRCLE,
+                           payload: circle})
+            })
     }
 }
