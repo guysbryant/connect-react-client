aa760946fded7bffba907ccde668f4b7b4fe0ed8
guysbryant <guy.s.bryant@gmail.com>
2021-06-19 17:08:22 -0400

    Put li inside of Link, apply style to Link

--- src/components/CircleListItem.jsx
+++ src/components/CircleListItem.jsx
@@ -3,9 +3,9 @@ import {Link} from 'react-router-dom'
 
 const CircleListItem = ({circle}) =>{
     return(
-        <li className="inline-flex flex m-2 text-white font-bold items-center rounded-full h-24 w-24 justify-center bg-blue-900" key={circle.id}>
-          <Link to={`/circles/${circle.id}`}>{circle.name}</Link>
-        </li>
+        <Link className="inline-flex flex m-2 text-white font-bold items-center rounded-full h-24 w-24 justify-center bg-blue-900" to={`/circles/${circle.id}`}>{circle.name}
+          <li  key={circle.id} />
+        </Link>
     )
 }
 
